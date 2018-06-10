b43faf04d890f8a42b30740ce42c6c889e81e085
zhangfangsong <zhangfangsong@adtime.com>
2018-06-10 17:34:18 +0800

    文章列表,导航

--- js/lara.js
+++ js/lara.js
@@ -7,6 +7,8 @@ var Lara = (function (){
 		pulldown : domain + 'api/v1/pulldown?include=user,category',
 		tags     : domain + 'api/v1/tags',
 		search   : domain + 'api/v1/search?include=user,category',
+		nav      : domain + 'api/v1/categories',
+		article  : domain + 'api/v1/article/',
 		getArticleList : function (dom, data, after){
 			for(var i=0; i<data.data.length; i++){
 				var div = document.createElement('div');
@@ -37,14 +39,29 @@ var Lara = (function (){
 				}
 			}
 		},
-		getDetail : function (aid){
+		getDetail : function (url, params){
 			return	{
-				url : 'detail.html',
-				id : 'detail.html',
+				url : url,
+				id :  url,
 				styles : {},
-				extras : {
-					aid : aid
+				extras : params,
+				show : {
+					autoShow : true,
+					aniShow : 'slide-in-right',
+					duration : 100
 				},
+				waiting : {
+					autoShow : true,
+					title : '正在加载...'
+				}
+			}
+		},
+		getList : function (url,params){
+			return	{
+				url : url,
+				id :  url,
+				styles : {},
+				extras : params,
 				show : {
 					autoShow : true,
 					aniShow : 'slide-in-right',
@@ -87,6 +104,48 @@ var Lara = (function (){
 				
 				li.innerHTML = htmlStr;
 				dom.appendChild(li);
+			}
+		},
+		getNavList : function (dom, data){
+			for(var i=0; i<data.data.length; i++){
+				var li = document.createElement("li");
+				li.className = "mui-table-view-cell";
+				li.setAttribute("catid", data.data[i].id);
+				li.setAttribute("catname", data.data[i].name);
+				
+				var htmlStr = '<a class="mui-navigate-right">' +data.data[i].name+ '</a>';
+				li.innerHTML = htmlStr;
+				
+				dom.appendChild(li);
+			}
+		},
+		getArticleDetail : function (dom, data){
+			document.querySelector(".article-thumb").src = data.thumb;
+			document.querySelector(".article-title").innerText = data.title;
+			document.querySelector(".article-author").innerText = data.user.username;
+			document.querySelector(".article-time").innerText = data.created_at;
+			document.querySelector(".article-article").innerHTML = data.content;
+			
+			if(data.comment.data.length){
+				for(var i=0; i<data.comment.data.length;i++){
+					var div = document.createElement("div");
+					div.className = "mui-card";
+					
+					var htmlStr = '<div class="mui-card-header mui-card-media" style="border: none;">';
+					htmlStr += '<img src="' +data.comment.data[i].user.avatar+ '" />';
+					htmlStr += '<div class="mui-media-body">' +data.comment.data[i].user.username+ '<p>' +data.comment.data[i].created_at+ '</p></div>';
+					htmlStr += '</div>';
+					htmlStr += '<div class="mui-card-content">' +data.comment.data[i].content+ '</div>';
+					
+					div.innerHTML = htmlStr;
+					dom.appendChild(div);
+				}
+			}else{
+				var div = document.createElement("div");
+				div.className = "no-comments";
+				div.innerHTML = "精彩评论虚位以待";
+				
+				dom.appendChild(div);
 			}
 		}
 	};
